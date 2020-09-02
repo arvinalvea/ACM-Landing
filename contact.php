4655f419e07a762da5dada2d5f1e707746a77121
Arvin Alvea <arvinalvea@gmail.com>
2020-09-02 09:23:00 -0400

    Updated files from Siteground files

--- contact.php
+++ contact.php
@@ -4,17 +4,17 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'arvinalvea@gmail.com';
+$from = 'info@acmconstructiongroup.ca';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'arvinalvea@gmail.com';
+$sendTo = 'acmconstructiongroup@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'email' => 'Email', 'location' => 'Location', 'need' => 'Project Type', 'message' => 'Message'); 
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
